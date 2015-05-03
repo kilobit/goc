d7d5103a53c96f697a7ef862aa2372f7fcefdd0b
Christian Saunders <cps@kilobit.ca>
2015-05-03 14:30:44 -0600

    Removed test http handlers used during the initial development.

--- goc.go
+++ goc.go
@@ -13,15 +13,6 @@ var (
 	path = flag.String("path", "/", "Root URL Path")
 )
 
-func fooHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Bar!")
-}
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-
-	w.Write([]byte("Hello World!"))
-}
-
 type CSVHandler struct {
 	reader *csv.Reader
 	rows [][]string
@@ -66,10 +57,6 @@ func main() {
 		mux.Handle(*path, http.StripPrefix(*path, http.FileServer(http.Dir(*root))))		
 	}
 	
-	mux.HandleFunc("/foo", fooHandler)
-	
-	mux.HandleFunc("/chew", rootHandler)
-
 	mux.Handle("/data", NewCSVHandler(os.Stdin))
 	
 	log.Fatal(http.ListenAndServe(":8080", mux))
