07fb605b8178337a87651f1606fc07bee87abffb
Christian Saunders <cps@kilobit.ca>
2015-08-02 15:29:36 -0600

    Modified CSVHandler to accept a writer and to switch on the input method.

--- goc.go
+++ goc.go
@@ -15,11 +15,14 @@ var (
 
 type CSVHandler struct {
 	reader *csv.Reader
+	writer *csv.Writer
 	rows [][]string
 }
 
-func NewCSVHandler (r io.Reader) *CSVHandler {
-	csvh := &CSVHandler{csv.NewReader(r), [][]string{}}
+func NewCSVHandler (r io.Reader, w io.Writer) *CSVHandler {
+	csvh := &CSVHandler{csv.NewReader(r), 
+		csv.NewWriter(w),
+		[][]string{}}
 
 	csvh.readCSVRows()
 
@@ -40,11 +43,36 @@ func (csvh *CSVHandler) readCSVRows() {
 
 func (csvh *CSVHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
+	switch {
+
+	case r.Method == "GET":
+		csvh.ReadCSV(w, r)
+		return
+
+	case r.Method == "POST":
+		csvh.WriteCSV(w, r)
+		return
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
+		io.WriteString(w, "The given method has not been implemented.")
+	}
+}
+
+func (csvh *CSVHandler) ReadCSV(w http.ResponseWriter, r *http.Request) {
+
 	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(http.StatusOK)
 	
 	jsone := json.NewEncoder(w)
 
 	jsone.Encode(csvh.rows)
+}
+
+func (csvh *CSVHandler) WriteCSV(w http.ResponseWriter, r *http.Request) {
+
+	
+
 }
 
 func main() {
@@ -57,7 +85,7 @@ func main() {
 		mux.Handle(*path, http.StripPrefix(*path, http.FileServer(http.Dir(*root))))		
 	}
 	
-	mux.Handle("/data", NewCSVHandler(os.Stdin))
+	mux.Handle("/data", NewCSVHandler(os.Stdin, os.Stdout))
 	
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
